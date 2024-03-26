1828bde8daa3ade11ef9b9e0dd79bb6edfc9c9d4
Nonthanisha <nonthanisha@gmail.com>
2024-03-26 11:39:16 +0700

    init project vue3 typeScript pinia tailwind

---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1 class="bg-violet-400">This is an
+      about page</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <div
+      class="bg-violet-400 h-96 text-white">
+      123456
+    </div>
+  </main>
+</template>
